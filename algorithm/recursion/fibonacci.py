a4e5d0d4b3bf10b87dfbe423c8962c11e210165b
HERYORDEJY <heryordejy.dev@gmail.com>
2024-04-11 17:40:41 +0100

    solved using Recursion method

--- algorithm/recursion/fibonacci.py
+++ algorithm/recursion/fibonacci.py
@@ -1,15 +1,27 @@
-def fibonacci(n):
-    fib_series = [1, 1]
+# def fibonacci(n):
+#     fib_series = [1, 1]
+#
+#     if n >= 3:
+#         for x in range(2, n):
+#             fib_series.append(fib_series[x - 1] + fib_series[x - 2])
+#
+#         return fib_series[n - 1]
+#
+#     elif n == 1 or n == 2:
+#         return 1
+#
+#
+# if __name__ == "__main__":
+#     print(fibonacci(2))
 
+
+def fib(n):
     if n >= 3:
-        for x in range(2, n):
-            fib_series.append(fib_series[x - 1] + fib_series[x - 2])
+        return fib(n - 1) + fib(n - 2)
 
-        return fib_series[n - 1]
-
-    elif n == 1 or n == 2:
+    else:
         return 1
 
 
 if __name__ == "__main__":
-    print(fibonacci(2))
+    print(fib(0))
